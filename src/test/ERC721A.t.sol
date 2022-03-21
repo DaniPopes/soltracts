f8226b0277a53f2e07a3f87b97bc716649d9f65b
DaniPopes <dani@danipopes.com>
2022-03-21 18:32:37 +0000

    change tests structure

--- src/test/ERC721A.t.sol
+++ src/test/ERC721A.t.sol
@@ -21,144 +21,76 @@ contract TestERC721A is BaseTest {
         vm.label(address(this), "TestERC721A");
     }
 
-    function testDeployGas() public {
-        unchecked {
-            new MockERC721A("abcdefg", "xyz", "https://example.com/12345/");
-        }
-    }
-
-    function testSafeMintGas1() public {
-        unchecked {
-            erc721a.safeMint(alice, 1);
-        }
-    }
-
-    function testSafeMintGas2() public {
-        unchecked {
-            erc721a.safeMint(alice, 2);
-        }
-    }
-
-    function testSafeMintGas3() public {
-        unchecked {
-            erc721a.safeMint(alice, 3);
-        }
-    }
-
-    function testSafeMintGas4() public {
-        unchecked {
-            erc721a.safeMint(alice, 4);
-        }
-    }
-
-    function testSafeMintGas5() public {
-        unchecked {
-            erc721a.safeMint(alice, 5);
-        }
-    }
-
-    function testSafeMintGasA() public {
-        unchecked {
-            erc721a.safeMint(alice, 10);
-        }
-    }
-
-    function testTransferFromGas() public {
-        address from = getRandomAddress(69420);
-        address to = getRandomAddress(420123);
-        vm.startPrank(from);
-
-        erc721a.safeMint(from, 2);
-
-        startMeasuringGas("First transfer");
-        erc721a.transferFrom(from, to, 1);
-        stopMeasuringGas();
-
-        assertEq(erc721a.balanceOf(from), 1);
-        assertEq(erc721a.balanceOf(to), 1);
-
-        startMeasuringGas("Second transfer");
-        erc721a.transferFrom(from, to, 2);
-        stopMeasuringGas();
-
-        assertEq(erc721a.balanceOf(from), 0);
-        assertEq(erc721a.balanceOf(to), 2);
-    }
-
-    function testSafeTransferFromGas() public {
-        address from = getRandomAddress(42069);
-        address to = getRandomAddress(69000);
-        vm.startPrank(from);
-
-        erc721a.safeMint(from, 2);
-
-        startMeasuringGas("First transfer");
-        erc721a.safeTransferFrom(from, to, 1);
-        stopMeasuringGas();
-
-        assertEq(erc721a.balanceOf(from), 1);
-        assertEq(erc721a.balanceOf(to), 1);
-
-        startMeasuringGas("Second transfer");
-        erc721a.safeTransferFrom(from, to, 2);
-        stopMeasuringGas();
-    }
-
-    function testSafeMint(uint256 amount) public {
-        vm.assume(amount > 0 && amount < type(uint128).max);
-        erc721a.safeMint(alice, amount);
-        assertEq(erc721a.balanceOf(alice), amount);
-    }
-
-    function testSafeMint() public {
+    function test_mint() public {
         uint256 amount = 5;
 
         vm.expectRevert(ERC721A.invalidRecipient.selector);
-        erc721a.safeMint(address(0), amount);
+        erc721a.mint(address(0), amount);
         vm.expectRevert(ERC721A.invalidAmount.selector);
-        erc721a.safeMint(alice, 0);
+        erc721a.mint(alice, 0);
 
-        erc721a.safeMint(alice, amount);
+        erc721a.mint(alice, amount);
         assertEq(erc721a.balanceOf(alice), amount);
     }
 
-    function testTotalSupply() public {
+    function test_safeMint() public {
+        uint256 startId = 1;
+        uint256 amount = 5;
+
+        // vm.expectCall(address(this), abi.encodeWithSignature("onERC721Received(address,address,uint256,bytes)"));
+        // strict match
+        for (uint256 i; i < amount; i++) {
+            vm.expectCall(
+                address(this),
+                abi.encodeWithSignature(
+                    "onERC721Received(address,address,uint256,bytes)",
+                    address(this),
+                    address(0),
+                    startId + i,
+                    ""
+                )
+            );
+        }
+        erc721a.safeMint(address(this), amount);
+    }
+
+    function test_totalSupply() public {
         uint256 startSupply = 0;
         uint256 amount = 5;
 
         assertEq(erc721a.totalSupply(), startSupply);
 
-        erc721a.safeMint(alice, amount);
+        erc721a.mint(alice, amount);
         assertEq(erc721a.totalSupply(), startSupply + amount);
 
         // TODO: add and test burn
     }
 
-    function testTokenOfOwnerByIndex() public {
+    function test_tokenOfOwnerByIndex() public {
         uint256 startId = 1;
         uint256 amount = 5;
 
         vm.expectRevert(ERC721A.invalidIndex.selector);
         erc721a.tokenOfOwnerByIndex(alice, 0);
 
-        erc721a.safeMint(alice, amount);
+        erc721a.mint(alice, amount);
         for (uint256 i; i < amount; i++) {
             assertEq(erc721a.tokenOfOwnerByIndex(alice, i), startId++);
         }
     }
 
-    function testTokenByIndex() public {
+    function test_tokenByIndex() public {
         uint256 startIndex = 1;
         uint256 amount = 1;
 
         vm.expectRevert(ERC721A.invalidIndex.selector);
         erc721a.tokenByIndex(startIndex);
 
-        erc721a.safeMint(alice, amount);
+        erc721a.mint(alice, amount);
         assertEq(erc721a.tokenByIndex(startIndex), startIndex);
     }
 
-    function testBalanceOf() public {
+    function test_balanceOf() public {
         // uint256 startId = 1;
         uint256 amount = 5;
 
@@ -166,11 +98,11 @@ contract TestERC721A is BaseTest {
         erc721a.balanceOf(address(0));
 
         assertEq(erc721a.balanceOf(alice), 0);
-        erc721a.safeMint(alice, amount);
+        erc721a.mint(alice, amount);
         assertEq(erc721a.balanceOf(alice), amount);
     }
 
-    function testOwnerOf() public {
+    function test_ownerOf() public {
         uint256 startId = 1;
         uint256 amount = 5;
 
@@ -179,9 +111,230 @@ contract TestERC721A is BaseTest {
         vm.expectRevert(ERC721.nonExistentToken.selector);
         erc721a.ownerOf(1);
 
-        erc721a.safeMint(alice, amount);
+        erc721a.mint(alice, amount);
         for (uint256 i; i < amount; i++) {
             assertEq(erc721a.ownerOf(startId + i), alice);
         }
+    }
+
+    function test_transferFrom() public {
+        vm.startPrank(alice);
+
+        // doesn't exist
+        vm.expectRevert(ERC721.nonExistentToken.selector);
+        erc721a.transferFrom(alice, bob, 0);
+        // not minted yet
+        vm.expectRevert(ERC721.nonExistentToken.selector);
+        erc721a.transferFrom(alice, bob, 1);
+
+        // mint id 1,2 to alice
+        erc721a.mint(alice, 2);
+        // not owner of ids
+        vm.stopPrank();
+        vm.startPrank(bob);
+        vm.expectRevert(ERC721A.wrongFrom.selector);
+        erc721a.transferFrom(bob, alice, 1);
+        vm.expectRevert(ERC721A.wrongFrom.selector);
+        erc721a.transferFrom(bob, alice, 2);
+        vm.stopPrank();
+
+        vm.startPrank(alice);
+        // assert balances and owners after mint
+        uint256 aliceBalance = erc721a.balanceOf(alice);
+        uint256 bobBalance = erc721a.balanceOf(bob);
+        address owner1 = erc721a.ownerOf(1);
+        address owner2 = erc721a.ownerOf(2);
+        assertEq(aliceBalance, 2);
+        assertEq(bobBalance, 0);
+        assertEq(owner1, alice);
+        assertEq(owner2, alice);
+
+        // xfr to address(0)
+        vm.expectRevert(ERC721A.invalidRecipient.selector);
+        erc721a.transferFrom(alice, address(0), 1);
+
+        // transfer id 1 to bob
+        erc721a.transferFrom(alice, bob, 1);
+
+        aliceBalance = erc721a.balanceOf(alice);
+        bobBalance = erc721a.balanceOf(bob);
+        owner1 = erc721a.ownerOf(1);
+        owner2 = erc721a.ownerOf(2);
+        assertEq(aliceBalance, 1);
+        assertEq(bobBalance, 1);
+        assertEq(owner1, bob);
+        assertEq(owner2, alice);
+
+        // transfer id 2 to bob
+        erc721a.transferFrom(alice, bob, 2);
+
+        aliceBalance = erc721a.balanceOf(alice);
+        bobBalance = erc721a.balanceOf(bob);
+        owner1 = erc721a.ownerOf(1);
+        owner2 = erc721a.ownerOf(2);
+        assertEq(aliceBalance, 0);
+        assertEq(bobBalance, 2);
+        assertEq(owner1, bob);
+        assertEq(owner2, bob);
+    }
+
+    function test_safeTransferFrom() public {
+        vm.startPrank(alice);
+
+        uint256 id = 1;
+        erc721a.mint(alice, 1);
+
+        vm.expectCall(
+            address(this),
+            abi.encodeWithSignature(
+                "onERC721Received(address,address,uint256,bytes)",
+                alice,
+                alice,
+                id,
+                ""
+            )
+        );
+        erc721a.safeTransferFrom(alice, address(this), id);
+    }
+
+    function test_approvals() public {
+        // mint id 1,2 to bob
+        erc721a.mint(bob, 2);
+
+        // bob approves alice to transfer id 2
+        vm.prank(bob);
+        erc721a.approve(alice, 2);
+
+        assertEq(erc721a.getApproved(1), address(0));
+        assertEq(erc721a.getApproved(2), alice);
+
+        vm.startPrank(alice);
+        // fail to transfer id 1 as not approved for it
+        vm.expectRevert(ERC721.notAuthorized.selector);
+        erc721a.transferFrom(bob, alice, 1);
+
+        // transfer id 2 using alice from bob to alice
+        erc721a.transferFrom(bob, alice, 2);
+
+        // check approval has been removed
+        assertEq(erc721a.getApproved(1), address(0));
+        assertEq(erc721a.getApproved(2), address(0));
+
+        // check token transferred correctly
+        uint256 aliceBalance = erc721a.balanceOf(alice);
+        uint256 bobBalance = erc721a.balanceOf(bob);
+        address owner1 = erc721a.ownerOf(1);
+        address owner2 = erc721a.ownerOf(2);
+        assertEq(aliceBalance, 1);
+        assertEq(bobBalance, 1);
+        assertEq(owner1, bob);
+        assertEq(owner2, alice);
+    }
+
+    function test_approvalForAll() public {
+        // mint id 1,2 to bob
+        erc721a.mint(bob, 2);
+
+        // bob approves alice for all ids
+        vm.prank(bob);
+        erc721a.setApprovalForAll(alice, true);
+
+        assertTrue(erc721a.isApprovedForAll(bob, alice));
+
+        vm.startPrank(alice);
+        // transfer id 1 using alice from bob to alice
+        erc721a.transferFrom(bob, alice, 1);
+
+        // check token transferred correctly
+        uint256 aliceBalance = erc721a.balanceOf(alice);
+        uint256 bobBalance = erc721a.balanceOf(bob);
+        address owner1 = erc721a.ownerOf(1);
+        address owner2 = erc721a.ownerOf(2);
+        assertEq(aliceBalance, 1);
+        assertEq(bobBalance, 1);
+        assertEq(owner1, alice);
+        assertEq(owner2, bob);
+
+        // revoke approval
+        vm.stopPrank();
+        vm.prank(bob);
+        erc721a.setApprovalForAll(alice, false);
+
+        assertFalse(erc721a.isApprovedForAll(bob, alice));
+
+        // now fails with the other token because not approved anymore
+        vm.startPrank(alice);
+        vm.expectRevert(ERC721.notAuthorized.selector);
+        erc721a.transferFrom(bob, alice, 2);
+    }
+
+    function testGas_deploy() public {
+        unchecked {
+            new MockERC721A("abcdefg", "xyz", "https://example.com/12345/");
+        }
+    }
+
+    function testGas_safeMint1() public {
+        unchecked {
+            erc721a.mint(alice, 1);
+        }
+    }
+
+    function testGas_safeMint2() public {
+        unchecked {
+            erc721a.mint(alice, 2);
+        }
+    }
+
+    function testGas_safeMint3() public {
+        unchecked {
+            erc721a.mint(alice, 3);
+        }
+    }
+
+    function testGas_safeMint4() public {
+        unchecked {
+            erc721a.mint(alice, 4);
+        }
+    }
+
+    function testGas_safeMint5() public {
+        unchecked {
+            erc721a.mint(alice, 5);
+        }
+    }
+
+    function testGas_safeMintA() public {
+        unchecked {
+            erc721a.mint(alice, 10);
+        }
+    }
+
+    function testGas_transferFrom() public {
+        vm.startPrank(alice);
+
+        erc721a.mint(alice, 2);
+
+        startMeasuringGas("First transfer");
+        erc721a.transferFrom(alice, bob, 1);
+        stopMeasuringGas();
+
+        startMeasuringGas("Second transfer");
+        erc721a.transferFrom(alice, bob, 2);
+        stopMeasuringGas();
+    }
+
+    function testGas_safeTransferFrom() public {
+        vm.startPrank(alice);
+
+        erc721a.mint(alice, 2);
+
+        startMeasuringGas("First transfer");
+        erc721a.safeTransferFrom(alice, bob, 1);
+        stopMeasuringGas();
+
+        startMeasuringGas("Second transfer");
+        erc721a.safeTransferFrom(alice, bob, 2);
+        stopMeasuringGas();
     }
 }
