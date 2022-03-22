a8a3c3b9a729c7209aed6a0ee167109011e7b50d
DaniPopes <dani@danipopes.com>
2022-03-22 07:55:21 +0000

    add more tests to ERC721ARoyalty.t.sol

--- src/test/ERC721ARoyalty.t.sol
+++ src/test/ERC721ARoyalty.t.sol
@@ -2,12 +2,12 @@
 pragma solidity ^0.8.4;
 
 import "./utils/BaseTest.sol";
-import { MockERC721Royalty } from "./utils/mocks/MockERC721Royalty.sol";
+import "./utils/mocks/MockERC721Royalty.sol";
 
 contract TestERC721Royalty is BaseTest {
     MockERC721Royalty private erc721aRoyalty;
 
-    function setUp() public {
+    function setUp() public virtual {
         erc721aRoyalty = new MockERC721Royalty();
 
         vm.label(address(erc721aRoyalty), "ERC721Royalty");
@@ -16,6 +16,11 @@ contract TestERC721Royalty is BaseTest {
 
     function test_royalty() public {
         vm.startPrank(alice);
+
+        vm.expectRevert(ERC721Royalty.InvalidReceiver.selector);
+        erc721aRoyalty.setRoyalty(address(0), 500);
+        vm.expectRevert(ERC721Royalty.InvalidRoyaltyFraction.selector);
+        erc721aRoyalty.setRoyalty(alice, 10001);
 
         // set receiver to alice and royalty to 5% (500bps)
         erc721aRoyalty.setRoyalty(alice, 500);
